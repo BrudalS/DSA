68df09d255b702ed09e214f5e3b737ada45bed17
BrudalS <123343570+BrudalS@users.noreply.github.com>
2024-05-12 22:51:12 +0530

    Added Linked List and Double linked list constructor

--- Double-Linked-List/DDL_constructor.py
+++ Double-Linked-List/DDL_constructor.py
@@ -1,11 +1,10 @@
-#  Costnructor
 class Node:
 	def __init__(self, value):
 		self.value = value
 		self.next = None
+		self.prev = None
 
-
-class LinkedList:
+class DobuleLinkedList:
 	def __init__(self, value):
 		new_node = Node(value)
 		self.head = new_node
